ab2bbf3ceae205173adb7556da88ed731e25948f
Haroon khan <haroonkhan9426@gmail.com>
2019-11-02 12:28:05 +0500

    Named Routes created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flash_chat/pages/home-page.dart';
+import 'package:flash_chat/pages/login-page.dart';
+import 'package:flash_chat/pages/chat-page.dart';
+import 'package:flash_chat/pages/register-page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/login': (context) => HomePage(),
+        '/register': (context) => RegisterPage(),
+        '/chat': (context) => ChatPage(),
+      },
+    );
   }
 }
--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
