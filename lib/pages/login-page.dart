704c6a29ba3a771aeae078f9bdbe0eb937284918
Haroon khan <haroonkhan9426@gmail.com>
2019-11-04 15:39:56 +0500

    Progress Indicator implemented in login and register page

--- lib/pages/login-page.dart
+++ lib/pages/login-page.dart
@@ -3,63 +3,80 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/custom_widgets/custom-textfield.dart';
 import 'package:flash_chat/custom_widgets/custom-rounded-button.dart';
 import 'package:flash_chat/services/firebase-helper.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email;
+  String password;
+  bool showProgressSpinner = false;
+
   @override
   Widget build(BuildContext context) {
-    String email;
-    String password;
     return Scaffold(
-        body: Container(
-      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Hero(
-            tag: 'flashIcon',
-            child: Icon(
-              Icons.flash_on,
-              color: Colors.amber[400],
-              size: 200.0,
+        body: ModalProgressHUD(
+      inAsyncCall: showProgressSpinner,
+      child: Container(
+        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Hero(
+              tag: 'flashIcon',
+              child: Icon(
+                Icons.flash_on,
+                color: Colors.amber[400],
+                size: 200.0,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          CustomTextField(
-            obscurText: false,
-            placeHolder: emailPlaceHolder,
-            onChange: (val) {
-              email = val;
+            SizedBox(
+              height: 20.0,
+            ),
+            CustomTextField(
+              obscurText: false,
+              placeHolder: emailPlaceHolder,
+              onChange: (val) {
+                email = val;
 //              print(val);
-            },
-          ),
-          CustomTextField(
-            obscurText: true,
-            placeHolder: passwordPlaceHolder,
-            onChange: (val) {
-              password = val;
+              },
+            ),
+            CustomTextField(
+              obscurText: true,
+              placeHolder: passwordPlaceHolder,
+              onChange: (val) {
+                password = val;
 //              print(val);
-            },
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          CustomRoundedButton(
-            text: loginButtonText,
-            color: lightBlue,
-            onPressed: () async{
-              var user = await FireBaseHelper()
-                  .login(email = email, password = password);
-              if(user != null){
-                print(user.email);
-                Navigator.pushNamed(context, chatPageId);
-              }else{
-                print('Login Failed');
-              }
-            },
-          ),
-        ],
+              },
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            CustomRoundedButton(
+              text: loginButtonText,
+              color: lightBlue,
+              onPressed: () async {
+                setState(() {
+                  showProgressSpinner = true;
+                });
+                var user = await FireBaseHelper()
+                    .login(email = email, password = password);
+                if (user != null) {
+                  print(user.email);
+                  Navigator.pushNamed(context, chatPageId);
+                } else {
+                  print('Login Failed');
+                }
+                setState(() {
+                  showProgressSpinner = false;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     ));
   }
--- lib/pages/register-page.dart
+++ lib/pages/register-page.dart
@@ -3,66 +3,81 @@ import 'package:flash_chat/custom_widgets/custom-rounded-button.dart';
 import 'package:flash_chat/custom_widgets/custom-textfield.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/services/firebase-helper.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  String email;
+  String password;
+  bool showProgressSpinner = false;
   @override
   Widget build(BuildContext context) {
-    String email;
-    String password;
-
     return Scaffold(
-        body: Container(
+        body: ModalProgressHUD(
+          inAsyncCall: showProgressSpinner,
+          child: Container(
       margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Hero(
-            tag: 'flashIcon',
-            child: Icon(
-              Icons.flash_on,
-              color: Colors.amber[400],
-              size: 200.0,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Hero(
+              tag: 'flashIcon',
+              child: Icon(
+                Icons.flash_on,
+                color: Colors.amber[400],
+                size: 200.0,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          CustomTextField(
-            obscurText: false,
-            placeHolder: emailPlaceHolder,
-            onChange: (val) {
-              email = val;
+            SizedBox(
+              height: 20.0,
+            ),
+            CustomTextField(
+              obscurText: false,
+              placeHolder: emailPlaceHolder,
+              onChange: (val) {
+                email = val;
 //              print(email);
-            },
-          ),
-          CustomTextField(
-            obscurText: true,
-            placeHolder: passwordPlaceHolder,
-            onChange: (val) {
-              password = val;
+              },
+            ),
+            CustomTextField(
+              obscurText: true,
+              placeHolder: passwordPlaceHolder,
+              onChange: (val) {
+                password = val;
 //              print(password);
-            },
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          CustomRoundedButton(
-            text: registerButtonText,
-            color: darkBlue,
-            onPressed: () async {
-              print('$email, $password');
-              var user = await FireBaseHelper()
-                  .registerUser(email = email, password = password);
-              if(user != null){
-                print(user.email);
-                Navigator.pushNamed(context, chatPageId);
-              }else{
-                print('User Registration Failed');
-              }
-            },
-          ),
-        ],
+              },
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            CustomRoundedButton(
+              text: registerButtonText,
+              color: darkBlue,
+              onPressed: () async {
+                setState(() {
+                  showProgressSpinner = true;
+                });
+                print('$email, $password');
+                var user = await FireBaseHelper()
+                    .registerUser(email = email, password = password);
+                if(user != null){
+                  print(user.email);
+                  Navigator.pushNamed(context, chatPageId);
+                }else{
+                  print('User Registration Failed');
+                }
+                setState(() {
+                  showProgressSpinner = false;
+                });
+              },
+            ),
+          ],
       ),
-    ));
+    ),
+        ));
   }
 }
